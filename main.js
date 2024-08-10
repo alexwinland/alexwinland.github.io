913c8214f0c8e86598037c74c85f995a81c343e7
Alex Rickey Winland <alex.winland@hotmail.com>
2024-08-10 15:20:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function getProjects(){
+    const urlGitHub = 'https://api.github.com/users/alexwinland/repos'
+    var loadingElement = document.getElementById('loading')
+
+    fetch(urlGitHub,{
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            showProjects(response)
+            loadingElement.style.display = 'none'
+        })
+        .catch((e) => {
+            console.log(`Error -> ${e}`)
+        })
+}
+
+function showProjects(data){
+    var listElement = document.getElementById('my-projects-list')
+    for(let i = 0; i < data.length; i++)
+    {
+        let div = document.createElement("div")
+        let a = document.createElement("a")
+        a.href = data[i]['clone_url']
+        a.target = '_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name']);
+        a.appendChild(linkText);
+        div.appendChild(a)
+        listElement.appendChild(div)
+    }
+}
+
+getProjects()
